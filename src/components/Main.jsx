069743e321356c4acb327cfb63768b34215d069b
FranVlahovic <franvlahovic@proton.me>
2025-03-23 01:31:52 +0100

    feat: Add React Icon & Update Main JSX and CSS Added React Icon for the background image of the body. Updated Main JSX by adding content inside (unordered list of React facts). Added Root Colors inside CSS. Stylized body section & main. Inside Main section stylized facts list and main background image.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,14 @@
 export default function Main(){
-    return <h1>Main goes here</h1>
+    return (
+        <main>
+            <h1>React Fun Facts</h1>
+            <ul className="facts-list">
+                <li>Was first released in 2023</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200k stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps </li>
+            </ul>
+        </main>
+    );
 };
